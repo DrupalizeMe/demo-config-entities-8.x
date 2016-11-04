a9339eb996478cd5507804222a9c67f2a7ce8e60
Amber Matz <amber@drupalize.me>
2016-11-04 15:20:13 -0700

    End of lesson 11. Generated module and creating settings form.

---
+++ docroot/modules/custom/transcode_profile/src/Form/AdminSettingsForm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\transcode_profile\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class AdminSettingsForm.
+ *
+ * @package Drupal\transcode_profile\Form
+ */
+class AdminSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'transcode_profile.AdminSettings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'admin_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('transcode_profile.AdminSettings');
+    $form['profile_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Profile Name'),
+      '#description' => $this->t('Video transcode profile name'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('profile_name'),
+    ];
+    $form['enable_transcoding'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable transcoding'),
+      '#description' => $this->t('Enables video transcoding'),
+      '#default_value' => $config->get('enable_transcoding'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('transcode_profile.AdminSettings')
+      ->set('profile_name', $form_state->getValue('profile_name'))
+      ->set('enable_transcoding', $form_state->getValue('enable_transcoding'))
+      ->save();
+  }
+
+}
